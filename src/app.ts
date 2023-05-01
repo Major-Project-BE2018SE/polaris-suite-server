9df3aa9910c7bc9f53b7fd5a90afd8dfc642d18d
alsongarbuja <garbuja12alson@gmail.com>
2023-05-01 21:41:27 +0545

    server: use middlewares like cors, passport, and connect to mongodb

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { Response } from 'express';
+import cors from 'cors';
+import passport from 'passport';
+import routes from './routes';
+import { jwtStrategy } from './config/passport';
+
+const app = express();
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+// app.use(xss());
+// app.use(mongoSanitize());
+
+// gzip compression
+// app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// jwt authentication
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
+
+// api routes
+app.use(routes);
+
+// send back a 404 error for any unknown api request
+app.use((_, res: Response, next) => {
+    next(res.status(404).send({ message: 'Not Found' }));
+});
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
-import express from 'express';
+import mongoose from 'mongoose';
+import { app } from './app';
+import { config } from './config/config';
+import { Server, IncomingMessage, ServerResponse } from 'http';
 
-const PORT = process.env.PORT || 8000;
-const app = express();
+let server: Server<typeof IncomingMessage, typeof ServerResponse>;
 
-app.get('/', (_, res) => {
-    res.send('Hello World!');
-    }
-);
+// connecting to mongodb using mongoose
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  console.log('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    console.log(`Listening to port ${config.port}`);
+  });
+});
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+// handling uncaught exceptions and unhandled rejections
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      console.log('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
 });
