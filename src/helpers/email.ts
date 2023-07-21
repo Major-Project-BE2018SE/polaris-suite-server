aef2e821a16904fa80955e7ff4bf7d92473ca6ac
Salipa-Gurung <salipagurung@gmail.com>
2023-07-21 21:15:52 +0545

    email: email template for verify email, reset password and project invitation

--- src/helpers/email.ts
+++ src/helpers/email.ts
@@ -1,33 +1,24 @@
 import nodemailer from 'nodemailer';
 import { config } from '../config/config';
 import { emailTypes } from '../config/constant';
+import { emailVerifyTemplate, passwordResetTemplate } from '../utils/emailTemplate';
 
 const transport = nodemailer.createTransport(config.email.smtp);
 
 export const sendEmail = async (email: string, token: string, type: string) => {
 
-    let url = '';
-    let sub = '';
-    let purpose = '';
-    let request = '';
+  let sub = '';
+  let message = '';
 
-    if(type === emailTypes.VERIFY_EMAIL) {
-        url = `${config.frontendUrl}/verify-email?token=${token}`;
-        sub = 'Email Verification for polaris suite';
-        purpose = 'verify your email';
-        request = 'create an account';
-    }
-    else if(type === emailTypes.RESET_PASSWORD) {
-        url = `${config.frontendUrl}/reset-password?token=${token}`;
-        sub = 'Password reset for polaris suite';
-        purpose = 'reset your password';
-        request = 'send a password reset request';
-    }
+  if(type === emailTypes.VERIFY_EMAIL) {
+    sub = 'Email Verification for polaris suite';
+    message = emailVerifyTemplate(token);    
+  }
+  else if(type === emailTypes.RESET_PASSWORD) {
+    sub = 'Password reset for polaris suite';
+    message = passwordResetTemplate(token);
+  }
 
-    const message =  `Dear user,
-    To ${purpose}, click on this link: <a href="${url}">${url}</a>
-    If you did not ${request}, then ignore this email.`
-
-    const msg = { from: config.email.from, to: email, subject: sub, html: message };
-    await transport.sendMail(msg);
+  const msg = { from: config.email.from, to: email, subject: sub, html: message };
+  await transport.sendMail(msg);
 }
