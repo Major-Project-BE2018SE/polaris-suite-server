5a4bd91e70930bbe48fedd31426908711e0d874b
Salipa-Gurung <salipagurung@gmail.com>
2023-08-24 22:21:31 +0545

    dashboard: add dashboard api

---
+++ src/routes/dashboard.route.ts
@@ -0,0 +1,40 @@
+import Joi from 'joi';
+import express from 'express';
+import type { Request, Response } from 'express';
+
+import { validate } from '../middlewares/validate';
+import { catchAsync } from '../helpers/catchAsync';
+import { objectId } from '../validations/custom.validation';
+import { ProjectModel, ShortcutModel, TestCaseModel } from '../models';
+import httpStatus from 'http-status';
+
+const router = express.Router();
+
+router.get('/:userId', 
+  validate({ 
+    params: Joi.object().keys({
+      userId: Joi.string().required().custom(objectId),
+    })
+  }), 
+  catchAsync(async (req: Request, res: Response) => {
+    const projects = await ProjectModel.find({ ownerID: req.params.userId });
+    let totalTestCases = 0;
+
+    for (let index = 0; index < projects.length; index++) {
+      const testcases = await TestCaseModel.find({ linkedProject: projects[index]._id });
+      totalTestCases += testcases.length;      
+    }
+
+    const shortcuts = await ShortcutModel.findOne({ userId: req.params.userId });
+
+    const dashboard = {
+      projects: projects,
+      totalReports: 0,
+      totalTestCases: totalTestCases,
+      shortcuts,
+    }
+
+    res.status(httpStatus.OK).json({ dashboard });
+  }));
+
+export default router;
